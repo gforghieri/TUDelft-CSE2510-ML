fc345525b0ce4818f29ff612b92f04e0c51b0f1f
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-09-19 22:11:44 +0200

    plot posterior

--- assignment_2/exercise5_2.py
+++ assignment_2/exercise5_2.py
@@ -1,5 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from exercise0_1 import iris
+from exercise5_1_new import posterior, means, sds, priors
+
+feature_idx = 2
 xs = np.linspace(0, 7, 100)
 # START ANSWER
+plt.plot(xs, posterior(xs, means, sds, priors, 0), label=iris.target_names[0])
+plt.plot(xs, posterior(xs, means, sds, priors, 1), label=iris.target_names[1])
+plt.plot(xs, posterior(xs, means, sds, priors, 2), label=iris.target_names[2])
 # END ANSWER
 plt.xlabel(iris.feature_names[feature_idx])
 plt.ylabel('Posterior probability')
