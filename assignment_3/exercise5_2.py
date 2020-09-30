d3465c5fea0c673af481358259898ebc5b27a9e7
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-01 00:12:20 +0200

    solve self accuracy check

--- assignment_3/exercise5_2.py
+++ assignment_3/exercise5_2.py
@@ -1,3 +1,6 @@
+from exercise5_1 import *
+
+
 def accuracy_score_self(Y_test, predictions):
     """
     Computes the accuracy of a test set as the fraction of items that was classified correctly.
@@ -8,6 +11,14 @@ def accuracy_score_self(Y_test, predictions):
 
     accuracy = 0
     # START ANSWER
+    correct_labels = []
+
+    for i in range(len(predictions)):
+        if predictions[i] == Y_test[i]:
+            correct_labels = np.append(correct_labels, predictions[i])
+
+    accuracy = len(correct_labels) / len(predictions)
+
     # END ANSWER
     return accuracy
 
