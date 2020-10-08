50fce718ba6dc84ee0da117ab17dfd0b10e2f04d
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-09 00:35:05 +0200

    apply gradient

--- assignment_4/exercise4_2.py
+++ assignment_4/exercise4_2.py
@@ -11,7 +11,7 @@ def calculate_gradients(theta, x, y):
     """
     gradients = np.zeros((len(x), len(theta)))
     # START ANSWER
-    temp = (y-hypothesis(x, theta))
+    temp = (y - hypothesis(x, theta))
     for i in range(len(x)):
         gradients[i] = temp[i] * x[i]
     return gradients
@@ -25,3 +25,28 @@ print(gradients)
 
 assert np.isclose(gradients[0], np.array([5.0, -2.5, -0.5])).all()
 assert np.isclose(gradients[1], np.array([0.00549347, 0.01098694, 0.01098694]), atol=0.0001).all()
+
+
+def apply_gradient(theta, gradient, alpha):
+    """
+    Applies the gradient step to theta and returns an adjusted theta.
+    :param theta: current theta array of size (d,)
+    :param gradient: gradient array of size (d,)
+    :param alpha: learning rate
+    :return: the updated theta array of size (d,)
+    """
+    updated_theta = theta
+    # START ANSWER
+    updated_theta += (alpha * gradient)
+
+    # END ANSWER
+    return updated_theta
+
+
+theta = np.array([1, 2, 3])
+gradient = np.array([10, -10, 5])
+alpha = 0.1
+updated_theta = apply_gradient(theta, gradient, alpha)
+print(updated_theta)
+
+assert (updated_theta == np.array([2, 1, 3.5])).all()
