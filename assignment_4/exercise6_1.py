48a9337c4c6d8e63605e7313911e5882e54094fd
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-10 21:13:22 +0200

    add exercise6_1 and 6_2

---
+++ assignment_4/exercise6_1.py
@@ -0,0 +1,35 @@
+from exercise5_3 import *
+
+# Import the load function for the dataset
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+n_classes = 10
+
+# Load the digits with 10 classes (digits 0 - 9)
+all_digits = datasets.load_digits(n_class=n_classes)
+all_digits_images = all_digits.images
+all_digits_labels = all_digits.target
+
+# flatten de data so they are 1D and append extra ones to the feature vectors
+all_digits_pixels = add_one_features(all_digits_images.reshape(all_digits_images.shape[0], -1))
+
+# the shape should be (1797, 65)
+print(all_digits_pixels.shape)
+
+# Split dataset into train and test set
+x_train_digits, x_test_digits, y_train_digits, y_test_digits = train_test_split(all_digits_pixels, all_digits_labels,
+                                                                                test_size=0.3)
+
+# initialize a theta array, one for every class
+multiclass_thetas = np.zeros((10, 65))
+
+for class_no in range(n_classes):
+    current_label = class_no
+    # Hint: convert the labels array to have only 1's at the current class_no
+    # START ANSWER
+    # END ANSWER
+    print("class_no: " + str(class_no))
+
+print("first 3 parameters of every theta")
+print(multiclass_thetas[:, :3])
---
+++ assignment_4/exercise6_2.py
@@ -0,0 +1,23 @@
+def predict_multiclass(x_test, theta):
+    """
+    Predicts a label for each image in x_test using theta.
+    :param x_test: an array of size (n, 65) of all test images.
+    :param theta: an (10,65) array of trained thetas.
+    :return: an array of size (n,) of labels for each test_image.
+    """
+    predictions = np.zeros(x_test.shape[0], dtype=int)
+    for i, x in enumerate(x_test):
+        # START ANSWER
+        # END ANSWER
+    return predictions
+
+predictions = predict_multiclass(x_test_digits, multiclass_thetas)
+# And print the accuracy
+accuracy = compute_accuracy(predictions, y_test_digits)
+print("accuracy: " + str(accuracy))
+
+assert accuracy > 0.9
+
+
+# for i in range(n_classes):
+#     plot_theta_image(multiclass_thetas[i], str(i))
