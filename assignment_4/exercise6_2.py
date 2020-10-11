516e908d8a2e10ea6100d0fcf8c239b3f8d2655c
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-11 13:31:57 +0200

    clean exercise 6_2

--- assignment_4/exercise6_2.py
+++ assignment_4/exercise6_2.py
@@ -1,5 +1,6 @@
 from exercise6_1 import *
 
+
 def predict_multiclass(x_test, theta):
     """
     Predicts a label for each image in x_test using theta.
@@ -14,25 +15,9 @@ def predict_multiclass(x_test, theta):
         for j in range(len(multiclass_thetas)):
             temp_predictions[j] = hypothesis(x, multiclass_thetas[j])
         predictions[i] = np.argmax(temp_predictions)
-
-
-        # return hypothesis function for x is label 0
-        # return hypothesis function for x is label 1
-        # return hypothesis function for x is label 2
-        # return hypothesis function for x is label 3
-        # return hypothesis function for x is label 4
-        # return hypothesis function for x is label 5
-        # return hypothesis function for x is label 6
-        # return hypothesis function for x is label 7
-        # return hypothesis function for x is label 8
-        # return hypothesis function for x is label 9
-        # temp_predictions = for each image in 540 x_test images an array of (10,) returned with predictions
-        # predictions = np.argmax(temp_predictions) will return the index of the label which is most likely the correct one
-        # thetas help assigning weight to each of the pixels, so that certain pixels matter more than others.
-        # predictions will be (540,)
-
         # END ANSWER
     return predictions
+
 
 predictions = predict_multiclass(x_test_digits, multiclass_thetas)
 # And print the accuracy
@@ -40,7 +25,6 @@ accuracy = compute_accuracy(predictions, y_test_digits)
 print("accuracy: " + str(accuracy))
 
 assert accuracy > 0.9
-
 
 for i in range(n_classes):
     plot_theta_image(multiclass_thetas[i], str(i))
