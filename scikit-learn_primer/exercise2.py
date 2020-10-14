90f9da3f519c93bda2a5a72dda3b9d0e8b5fc5c7
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-14 23:41:37 +0200

    check accuracy scores

---
+++ scikit-learn_primer/exercise2.py
@@ -0,0 +1,20 @@
+from exercise1 import *
+from sklearn.utils.validation import check_is_fitted
+
+for name, model in models.items():
+    # START ANSWER
+    model.fit(X, y)
+    # END ANSWER
+
+for model in models.values():
+    check_is_fitted(model)
+
+from sklearn.metrics import f1_score, accuracy_score
+
+for name, model in models.items():
+    prediction = model.predict(X)
+    f1_score_value = f1_score(prediction, y, average="weighted")
+    accuracy = accuracy_score(prediction, y)
+    print(name)
+    print("- accuracy_score", accuracy)
+    print("- f1_score", f1_score_value)
