cbb19165b7881fb5aebd3527ad651649330c5912
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-19 01:00:52 +0200

    starting MNIST

---
+++ final_assignment/MNIST/1_imports.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.dummy import DummyClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+
+import matplotlib.pyplot as plt
---
+++ final_assignment/MNIST/3_load_data.py
@@ -0,0 +1,6 @@
+mnist_28x28_train = np.load("mnist_28x28_train.npy")
+mnist_8x8_train = np.load("mnist_8x8_train.npy")
+train_labels = np.load("train_labels.npy")
+
+mnist_28x28_test = np.load("mnist_28x28_test.npy")
+mnist_8x8_test = np.load("mnist_8x8_test.npy")
