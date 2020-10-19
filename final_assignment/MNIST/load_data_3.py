cf814c5c56e1692d9d7c7d75363215f5496802ff
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-10-19 14:39:41 +0200

    load data

--- final_assignment/MNIST/load_data_3.py
+++ final_assignment/MNIST/load_data_3.py
@@ -1,8 +1,8 @@
 from imports_1 import *
 
-mnist_28x28_train = np.load("mnist_28x28_train.npy")
-mnist_8x8_train = np.load("mnist_8x8_train.npy")
-train_labels = np.load("train_labels.npy")
+mnist_28x28_train = np.load("data/mnist_28x28_train.npy")
+mnist_8x8_train = np.load("data/mnist_8x8_train.npy")
+train_labels = np.load("data/train_labels.npy")
 
-mnist_28x28_test = np.load("mnist_28x28_test.npy")
-mnist_8x8_test = np.load("mnist_8x8_test.npy")
+mnist_28x28_test = np.load("data/mnist_28x28_test.npy")
+mnist_8x8_test = np.load("data/mnist_8x8_test.npy")
