b618d1a5f8c3e041b5cf9765f71c67377deb15cf
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-11-01 14:09:56 +0100

    solve double exponential distribution

--- final_exam_codingprep/double_exponential_distribution.py
+++ final_exam_codingprep/double_exponential_distribution.py
@@ -9,5 +9,12 @@ class Solution():
         beta1 = 1.0
         beta2 = 2.0
 
-        py1_given_x = 0
+        px_giveny1 = (1 / (2 * beta1)) * np.exp(((-np.abs((testx - mu1) / beta1))))
+        px_giveny2 = (1 / (2 * beta2)) * np.exp(((-np.abs((testx - mu2) / beta2))))
+
+        py1 = 0.5
+        py2 = 0.5
+
+        py1_given_x = px_giveny1 * py1 / (px_giveny1 * py1 + px_giveny2 * py2)
+
         return py1_given_x
--- final_exam_codingprep/double_exponential_distribution_test.py
+++ final_exam_codingprep/double_exponential_distribution_test.py
@@ -1,13 +1,10 @@
 import unittest
-from solution import Solution
-import weblabTestRunner
+
+from double_exponential_distribution import Solution
 
 
 class TestSolution(unittest.TestCase):
 
-  def test_xis0(self):
-    sol = Solution.solution(0.)
-    self.assertAlmostEqual(sol, 0.8996324353165482, places=2)
-
-if __name__ == "__main__":
-  unittest.main(testRunner=weblabTestRunner.TestRunner)
+    def test_xis0(self):
+        sol = Solution.solution(0.)
+        self.assertAlmostEqual(sol, 0.8996324353165482, places=2)
